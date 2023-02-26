88b43efe457f11b935079765b5fcb26f1a8516aa
sebama94 <umeest@gmail.com>
2023-02-26 20:51:44 +0100

    update and fix

--- package/ModelSelector.py
+++ package/ModelSelector.py
@@ -4,7 +4,7 @@ from keras.layers import Dense
 class ModelSelector:
     def __init__(self, input_shape):
         self.model = Sequential()
-        self.model.add(Dense(32, input_shape=(input_shape,), activation='relu'))
-        self.model.add(Dense(32, activation='relu'))
-        self.model.add(Dense(units=32, activation='sigmoid'))
-        self.model.add(Dense(32, activation='softmax'))
+        self.model.add(Dense(10, input_shape=(input_shape,), activation='relu'))
+        self.model.add(Dense(10, activation='relu'))
+        self.model.add(Dense(1, activation='sigmoid'))
+
