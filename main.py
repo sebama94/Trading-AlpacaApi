bcedd2228aa98c27404d3e885a73e7c6a40d8dc4
sebama94 <umeest@gmail.com>
2023-03-01 20:12:58 +0100

    update and fix

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 from Utilies.utilies import dict_credential
 from package.Deployer import Deployment
-import os
 import tensorflow as tf
 
-import tensorflow as tf
+# from tensorflow.python.client import device_lib
+# print(device_lib.list_local_devices())
 
-physical_devices = tf.config.list_physical_devices('/GPU:0')
+physical_devices = tf.config.list_physical_devices('GPU')
 print("Num GPUs Available: ", len(physical_devices))
 
-os.environ['KMP_DUPLICATE_LIB_OK'] = 'TRUE'
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
 # Set API credentials
 API_KEY = dict_credential["API_KEY"]
 API_SECRET = dict_credential["API_SECRET"]
@@ -19,7 +18,7 @@ APCA_API_BASE_URL = dict_credential["APCA_API_BASE_URL"]
 # api = tradeapi.REST(API_KEY, API_SECRET, APCA_API_BASE_URL, api_version='v2')
 
 def main(api_key, secret_key, apca_api_base_url, symbol):
-    # with tf.device('/GPU:0'):
+    with tf.device('/device:GPU:0'):
         # Create deployment object
         deployment = Deployment(api_key, secret_key, apca_api_base_url, symbol)
 
@@ -27,12 +26,10 @@ def main(api_key, secret_key, apca_api_base_url, symbol):
         deployment.deploy_model()
 
 
-
-
 if __name__ == '__main__':
     api_key = API_KEY
     secret_key = API_SECRET
     apca_api_base_url = APCA_API_BASE_URL
-    symbol = "AMZN"
+    symbol = "AAPL"
 
     main(api_key, secret_key, apca_api_base_url, symbol)
