46057dc0173d320b990e5ab26d817569d1135cd1
sebama94 <umeest@gmail.com>
2023-03-09 21:45:38 +0100

    update and fix

--- package/DeployerCrypto.py
+++ package/DeployerCrypto.py
@@ -1,10 +1,26 @@
 from package.Deployer import Deployment
 from alpaca_trade_api import TimeFrame, TimeFrameUnit
 import time
+import numpy as np
+
 
 class DeployementCrypto(Deployment):
     def __init__(self, symbol, api):
         Deployment.__init__(self, symbol, api)
+
+    def deploy_model(self):
+        try:
+            # real_price = float(self.alpaca_trade_api.get_position(self.symbol).current_price)
+            real_price = float(self.alpaca_trade_api.get_crypto_bars(self.symbol, timeframe= TimeFrame(15, TimeFrameUnit.Minute))[-1].c)
+            # Make prediction
+            prediction = self.model.predict(np.array([[real_price]]))
+            print(f"prediction {prediction} real price {real_price}")
+            self.submit_order(prediction=prediction, real_price= real_price)
+            time.sleep(20)
+        except Exception as exception:
+            print(f"[ERROR] exception {exception}. Symbol {self.symbol}")
+            time.sleep(2)
+            # real_price = 140
 
     def collect_data(self):
         data = self.alpaca_trade_api.get_crypto_bars(self.symbol, timeframe= TimeFrame(15, TimeFrameUnit.Minute),
