324a9ec2443bf3809e6049c58155c12c1c652520
sebama94 <umeest@gmail.com>
2024-10-07 20:43:06 +0200

    added utilies.py for login and start and end data

---
+++ Utilies/utilies.py
@@ -0,0 +1,10 @@
+dict_credential = {
+    "API_KEY": ' INSERT HERE ',
+    "API_SECRET": ' INSERT HERE ',
+    "APCA_API_BASE_URL" : 'https://paper-api.alpaca.markets'
+}
+
+data_analysing = {
+    "START_DATA" : '2023-11-01',
+    "END_DATE" : '2023-11-17'
+}
